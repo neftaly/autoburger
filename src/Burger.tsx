868fe79f234760fb227aa7081c6486b1123872fc
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2023-06-02 11:27:28 -0700

    Use flexbox

--- src/Burger.tsx
+++ src/Burger.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@react-three/drei";
+import { Flex, Box as FlexBox } from "@react-three/flex";
 
 export const toppings = {
   bun: { color: "yellow", size: [1.2, 0.2, 1.2] },
@@ -6,16 +7,16 @@ export const toppings = {
   lettuce: { color: "green", size: [1.1, 0.05, 1.1] },
 };
 
-export const Layer = ({ color, size, position }) => (
-  <Box args={size} material-color={color} position={position} />
+export const Layer = ({ color, size }) => (
+  <FlexBox centerAnchor>
+    <Box args={size} material-color={color} />
+  </FlexBox>
 );
 
-export const Burger = ({ layers }) => {
-  const [sliceData, burgerWidth] = layers.reduce(
-    ([s, offset], slice) => [[...s, [offset, slice]], offset + slice.size[1]],
-    [[], 0]
-  );
-  return sliceData.map(([offset, slice], key) => (
-    <Layer key={key} {...slice} position={[0, offset + slice.size[1] / 2, 0]} />
-  ));
-};
+export const Burger = ({ layers }) => (
+  <Flex justifyContent="center" alignItems="center">
+    {layers.map((layer, key) => (
+      <Layer key={key} {...layer} />
+    ))}
+  </Flex>
+);
