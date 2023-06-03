8d50803f1b3f6356ac1ca44479b45778fe7f0183
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2023-06-03 09:22:41 -0700

    Cleanup

--- src/Burger.tsx
+++ src/Burger.tsx
@@ -2,16 +2,19 @@ import { Box } from "@react-three/drei";
 import { Flex, Box as FlexBox } from "@react-three/flex";
 
 export const toppings = {
-  bun: { color: "yellow", size: [1.2, 0.2, 1.2] },
-  patty: { color: "brown", size: [1, 0.2, 1] },
-  lettuce: { color: "green", size: [1.1, 0.05, 1.1] },
+  bun: { color: "yellow", args: [1.2, 0.2, 1.2] },
+  patty: { color: "brown", args: [1, 0.2, 1] },
+  lettuce: { color: "green", args: [1.1, 0.05, 1.1] },
 };
 
-export const Layer = ({ type }) => (
-  <FlexBox centerAnchor>
-    <Box args={toppings[type].size} material-color={toppings[type].color} />
-  </FlexBox>
-);
+export const Layer = ({ type }) => {
+  const { args, color } = toppings[type];
+  return (
+    <FlexBox centerAnchor>
+      <Box args={args} material-color={color} />
+    </FlexBox>
+  );
+};
 
 export const Burger = ({ layers }) => (
   <Flex justifyContent="center" alignItems="center">
