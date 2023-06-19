846d71151aa06045aea3233e21ad680326e2427d
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2023-06-20 07:59:45 +1200

    Add default layers

--- src/Burger.tsx
+++ src/Burger.tsx
@@ -17,7 +17,7 @@ export const Layer = ({ type }) => {
 };
 
 // As we're building from the bottom up, reverse the layer order
-export const Burger = ({ layers }) => (
+export const Burger = ({ layers = [] }) => (
   <Flex justifyContent="center" alignItems="center" position={[-0.5, 0, 0]}>
     {layers.map((type, key) => <Layer key={key} type={type} />).reverse()}
   </Flex>
