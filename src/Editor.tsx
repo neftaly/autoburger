dc64ad6d10b1095c8117b2029a6b1a05db3d0d8d
jermsam <ssali@jitpomi.com>
2023-06-25 18:12:29 -0600

    removed cursor:move

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -18,7 +18,6 @@ export const Editor = ({doc, changeDoc}) => {
             <div id="list" style={{minWidth: '290px'}}>
                 {doc?.layers.map((layer, key) => (
                     <div key={key} style={{
-                        cursor: 'move',
                         userSelect: 'none',
                         display: 'flex',
                         justifyContent: 'space-between',
